9193d91ee9c5c2b3828c366c21ce6bde7f17ac2d
Sameer <samirsaini99@gmail.com>
2023-05-01 10:33:13 +0530

    Dummy files removed, keywords added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang='en'>
+            <Head>
+                <title>Parkway Adult Family Homecare</title>
+                <meta name='title' content='Parkway Adult Family Homecare' />
+                <meta
+                    name='description'
+                    content='Parkway Adult Family Homecare is a residential care facility that offers long-term care for elderly or disabled individuals in a home-like setting. Our purpose is to provide potential residents and their families with a comprehensive understanding of the care offered at the home.'
+                />
+                <meta
+                    name='keywords'
+                    content='parkway,adult,family,homecare,oldage,home,homes,old,people,care'
+                />
+                <meta name='robots' content='index, follow' />
+                <meta name='language' content='English' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
