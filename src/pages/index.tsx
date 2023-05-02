ce42168fb75788f8a079c0fc645b7a4643c144fd
Sameer <samirsaini99@gmail.com>
2023-05-02 11:52:32 +0530

    Our Mission section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Sl_Image1 from '@/assets/images/moving_slide1.jpg';
 import Sl_Image2 from '@/assets/images/moving_slide2.jpg';
 import Sl_Image3 from '@/assets/images/moving_slide3.jpg';
 import Sl_Image4 from '@/assets/images/moving_slide4.jpg';
+import QuadPic from '@/assets/images/quad_pic.jpg';
 import SmilingMan from '@/assets/images/smiling_man.png';
 import Header from '@/components/common/Header';
 import SlideImage from '@/components/page/home/SlideImage';
@@ -95,6 +96,33 @@ const Home = () => {
                     <SlideImage img={Sl_Image3} text='Lorem ipsum temipsum' />
                     <SlideImage img={Sl_Image4} text='Lorem ipsum temipsum' />
                 </div>
+            </section>
+            <section className={styles.mission}>
+                <Row className='g-0'>
+                    <Col className={styles.mission_details} md={6}>
+                        <h1>Our Mission</h1>
+                        <p>
+                            The mission and intent of PARKWAY{' '}
+                            <span>ADULT FAMILY HOMECARE</span> is to make a
+                            positive difference in every life we touch,
+                            promoting health, safety and individual rights. Our
+                            home will provide housing for up to 6 adult persons
+                            who are unable to live independently within the
+                            community. Residents will be able to decorate and
+                            furnish their own living spaces in a way that
+                            reminds them of their own home complete with family
+                            pictures. We will offer both individual rooms as
+                            well as double occupancy .{' '}
+                            <span className={styles.link}>Know More</span>
+                        </p>
+                    </Col>
+                    <Col
+                        md={6}
+                        className='d-flex justify-content-center justify-content-md-end'
+                    >
+                        <Image src={QuadPic} alt='Quad' className='img-fluid' />
+                    </Col>
+                </Row>
             </section>
         </>
     );
